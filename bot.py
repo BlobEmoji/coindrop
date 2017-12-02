0df9e226d655f9a55296123aa5501d1090d34995
Gorialis <sansgorialis@gmail.com>
2017-12-03 02:07:00 +0900

    Move logger and add proper error handling

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@
 
 import asyncio
 import asyncpg
+import hashlib
 import logging
+import traceback
 
+import discord
 from discord.ext import commands
-
-logger = logging.getLogger("dropbot")
 
 
 class DropBot(commands.Bot):
@@ -16,6 +17,7 @@ class DropBot(commands.Bot):
         self.config = config or {}
         self.db = None
         self.db_available = asyncio.Event()
+        self.logger = logging.getLogger("dropbot")
 
         self.loop.create_task(self.acquire_pool())
 
@@ -23,9 +25,27 @@ class DropBot(commands.Bot):
         credentials = self.config.pop("db_credentials")
 
         if credentials is None:
-            logger.critical("Cannot connect to db, no credentials!")
+            self.logger.critical("Cannot connect to db, no credentials!")
             await self.logout()
 
         self.db = await asyncpg.create_pool(**credentials)
         self.db_available.set()
 
+    async def on_command_error(self, ctx: commands.Context, exception):
+        msg = ctx.message
+        if isinstance(exception, (commands.CommandOnCooldown, commands.CommandNotFound,
+                                  commands.DisabledCommand, commands.MissingPermissions,
+                                  commands.CheckFailure)):
+            pass  # we don't care about these
+        elif isinstance(exception, (commands.BadArgument, commands.MissingRequiredArgument)):
+            try:
+                await msg.add_reaction("\N{BLACK QUESTION MARK ORNAMENT}")
+            except discord.HTTPException:
+                pass
+        else:
+            error_digest = "".join(traceback.format_exception(type(exception), exception,
+                                                              exception.__traceback__, 8))
+            error_hash = hashlib.sha256(error_digest.encode("utf8")).hexdigest()
+            short_hash = error_hash[0:8]
+            self.logger.error(f"Encountered command error [{error_hash}] ({msg.id}):\n{error_digest}")
+            await ctx.send(f"Uh-oh, that's an error [{short_hash}...]")
